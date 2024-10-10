fbc544599439b82929d8f5f4d0abc017314f1307
Jatin-Puri <jatinpuri11@gmail.com>
2024-10-10 16:41:09 +1300

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/jatin-puri-coder/jatin-puri',
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
